16f9b3eadce24c7cc4a081135001ed1e327ff743
jsyaklin <foowheely@gmail.com>
2025-12-07 10:18:36 -0600

    imports

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import vapory as vp
+import elastica as ea
+from collections import defaultdict
+
+# np.set_printoptions(precision=5, suppress=True, threshold=6) # TODO
+np.set_printoptions(precision=5, suppress=False, threshold=np.inf) # TODO
+
 print('hello world')
